65bfdf209fd17d1bd7402d415ae8974f6f10cce2
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-03-19 16:41:47 +0530

    Added subscription support

--- server/createServer.js
+++ server/createServer.js
@@ -1,20 +1,45 @@
-import express from 'express'
-const { GraphQLServer } = require('graphql-yoga')
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 
 export default () => {
   const typeDefs = `
-      type Query {
-        hello(name: String): String!
-      }
-    `
+  type Query {
+    hello: String!
+  }
+  type Counter {
+    count: Int!
+    countStr: String
+  }
+  type Subscription {
+    counter: Counter!
+  }
+`
 
   const resolvers = {
     Query: {
-      hello: (_, { name }) => `Hello ${name || 'World'}`,
+      hello: () => `Hello`,
+    },
+    Counter: {
+      countStr: counter => `Current count: ${counter.count}`,
+    },
+    Subscription: {
+      counter: {
+        subscribe: (parent, args, { pubsub }) => {
+          const channel = Math.random()
+            .toString(36)
+            .substring(2, 15) // random channel name
+          let count = 0
+          setInterval(
+            () => pubsub.publish(channel, { counter: { count: count++ } }),
+            2000,
+          )
+          return pubsub.asyncIterator(channel)
+        },
+      },
     },
   }
 
-  const server = new GraphQLServer({ typeDefs, resolvers })
+  const pubsub = new PubSub()
+  const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } })
 
   server.start(() => console.log('Server is running on localhost:4000'))
 }
--- server/index.js
+++ server/index.js
@@ -3,5 +3,4 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
 const app = createServer()
