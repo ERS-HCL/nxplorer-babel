d1addbf667c51c4c0349d5c00a4926dff368718b
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-03-20 20:05:45 +0530

    Env controlled server options

--- server/createServer.js
+++ server/createServer.js
@@ -9,5 +9,20 @@ export default () => {
     context: { pubsub }
   })
 
-  server.start(() => console.log('Server is running on localhost:4000'))
+  const port = process.env.GRAPHQL_PORT || 3000
+  const tracing = process.env.GRAPHQL_TRACING || false
+  const debug = process.env.GRAPHQL_DEBUG || false
+  const playground = process.env.GRAPHQL_PLAYGROUND || '/'
+  const options = {
+    port,
+    debug,
+    playground,
+    tracing
+  }
+
+  server.start(options, ({ port, tracing }) =>
+    console.log(
+      `Server started, listening on port ${port} for incoming requests. Tracing : ${tracing} `
+    )
+  )
 }
